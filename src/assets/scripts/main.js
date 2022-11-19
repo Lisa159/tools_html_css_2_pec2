3eaa36a88f873fd19b9c66a058043fc30452116b
Aida Maria Gonzalez Fernandez <amgonzalez96@uoc.edu>
2022-11-19 12:28:28 +0100

    Reestructured files

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,6 +11,26 @@ import * as bootstrap from "bootstrap";
 bootstrap;
 
 +(function () {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
+  let loc = window.location.pathname;
+  _removeActiveLinks();
+  if (loc.includes("profile")) {
+    document.getElementsByClassName("nav-link")[0];
+    document.getElementsByClassName("nav-link")[0].ariaCurrent = "page";
+    document.getElementsByClassName("nav-link")[0].classList.add("active");
+  } else if (loc.includes("festival")) {
+    document.getElementsByClassName("nav-link")[1];
+    document.getElementsByClassName("nav-link")[1].ariaCurrent = "page";
+    document.getElementsByClassName("nav-link")[1].classList.add("active");
+  } else if (loc.includes("gallery")) {
+    document.getElementsByClassName("nav-link")[2];
+    document.getElementsByClassName("nav-link")[2].ariaCurrent = "page";
+    document.getElementsByClassName("nav-link")[2].classList.add("active");
+  }
 })();
+
+function _removeActiveLinks() {
+  [0, 1, 2].map((el) => {
+    document.getElementsByClassName("nav-link")[el].ariaCurrent = null;
+    document.getElementsByClassName("nav-link")[el].classList.remove("active");
+  });
+}
