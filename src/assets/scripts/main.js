115235a8e80461404f4f25aff7edd9574081b657
Aida Maria Gonzalez Fernandez <amgonzalez96@uoc.edu>
2022-11-21 20:22:12 +0100

    Commented js

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,35 +2,34 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-import * as bootstrap from "bootstrap";
 
 /**
  * Write any other JavaScript below
  */
 
-bootstrap;
+// bootstrap;
 
-(function () {
-  let loc = window.location.pathname;
-  _removeActiveLinks();
-  if (loc.includes("profile")) {
-    document.getElementsByClassName("nav-link")[0];
-    document.getElementsByClassName("nav-link")[0].ariaCurrent = "page";
-    document.getElementsByClassName("nav-link")[0].classList.add("active");
-  } else if (loc.includes("festival")) {
-    document.getElementsByClassName("nav-link")[1];
-    document.getElementsByClassName("nav-link")[1].ariaCurrent = "page";
-    document.getElementsByClassName("nav-link")[1].classList.add("active");
-  } else if (loc.includes("contact")) {
-    document.getElementsByClassName("nav-link")[2];
-    document.getElementsByClassName("nav-link")[2].ariaCurrent = "page";
-    document.getElementsByClassName("nav-link")[2].classList.add("active");
-  }
-})();
+// (function () {
+//   let loc = window.location.pathname;
+//   _removeActiveLinks();
+//   if (loc.includes("profile")) {
+//     document.getElementsByClassName("nav-link")[0];
+//     document.getElementsByClassName("nav-link")[0].ariaCurrent = "page";
+//     document.getElementsByClassName("nav-link")[0].classList.add("active");
+//   } else if (loc.includes("festival")) {
+//     document.getElementsByClassName("nav-link")[1];
+//     document.getElementsByClassName("nav-link")[1].ariaCurrent = "page";
+//     document.getElementsByClassName("nav-link")[1].classList.add("active");
+//   } else if (loc.includes("contact")) {
+//     document.getElementsByClassName("nav-link")[2];
+//     document.getElementsByClassName("nav-link")[2].ariaCurrent = "page";
+//     document.getElementsByClassName("nav-link")[2].classList.add("active");
+//   }
+// })();
 
-function _removeActiveLinks() {
-  [0, 1, 2].map((el) => {
-    document.getElementsByClassName("nav-link")[el].ariaCurrent = null;
-    document.getElementsByClassName("nav-link")[el].classList.remove("active");
-  });
-}
+// function _removeActiveLinks() {
+//   [0, 1, 2].map((el) => {
+//     document.getElementsByClassName("nav-link")[el].ariaCurrent = null;
+//     document.getElementsByClassName("nav-link")[el].classList.remove("active");
+//   });
+// }
